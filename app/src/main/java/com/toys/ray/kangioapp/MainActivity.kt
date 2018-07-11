8b2c293d3cb5bdcb589d434a4a088c8347abfe0b
Ray <ray@linux-zyk1.nj.trantect.com>
2018-07-11 18:26:00 +0800

    Initial commit

---
+++ app/src/main/java/com/toys/ray/kangioapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.toys.ray.kangioapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
